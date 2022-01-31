eb608c08e2bf328c27b691ecea2864cbe562f4b4
SIDDARTH.D <siddarth.dayasagar@gmail.com>
2022-01-31 09:25:32 +0530

    Update pick_obj.cpp

--- rusty/src/pick_obj.cpp
+++ rusty/src/pick_obj.cpp
@@ -1,62 +1,69 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
-#include <iostream>
+#include <std_msgs/UInt8.h>
 
-// Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-float pose(float pose)
-{
-  MoveBaseClient ac("move_base", true);
-  //float pose_arr[2]={-15.0,10.0};
 
+int main(int argc, char** argv){
+  // Initialize the simple_navigation_goals node
+  ros::init(argc, argv, "pick_objects");
+  ros::NodeHandle n;
   
+  MoveBaseClient ac("move_base", true);
+  //set up publisher to broadcast if robot is at goal marker
+  ros::Publisher pub = n.advertise<std_msgs::UInt8>("/goal", 1);
   // Wait 5 sec for move_base action server to come up
-  while(!ac.waitForServer(ros::Duration(5.0)))
-  {
+  while(!ac.waitForServer(ros::Duration(5.0))){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
 
-  move_base_msgs::MoveBaseGoal goal;
+  move_base_msgs::MoveBaseGoal goal;	
+  std_msgs::UInt8 delivery_data;
+  ROS_INFO("PARAM loaded");
   goal.target_pose.header.frame_id = "map";
   goal.target_pose.header.stamp = ros::Time::now();
-
-  ROS_INFO("GOING  TO POSITION %f",pose);
-  goal.target_pose.pose.position.x = pose;
-  goal.target_pose.pose.orientation.w = 1.0;
-    
-    
   
+  n.getParam("/pick_up/x", goal.target_pose.pose.position.x);
+  n.getParam("/pick_up/w", goal.target_pose.pose.orientation.w);
 
-   // Send the goal position and orientation for the robot to reach
-  ROS_INFO("Sending goal to rusty");
   ac.sendGoal(goal);
 
-  // Wait an infinite time for the results
   ac.waitForResult();
 
-  // Check if the robot reached its goal
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Rusty as reached");
-  else
-    ROS_INFO("Rusty could not reach ");
+  if (ac.getState()==actionlib::SimpleClientGoalState::SUCCEEDED)
+  {
+    delivery_data.data = 1;
+    pub.publish(delivery_data);
+    ROS_INFO("PICK UP REACHED");
+  }
+  else{
+    delivery_data.data = 2;
+    pub.publish(delivery_data);
+    ROS_INFO("Rusty did not reach the location");
+  }
 
-  return 0;
-}
+  //DROP Sequence
+  ros::Duration(5).sleep();
 
+  n.getParam("/drop/x", goal.target_pose.pose.position.x);
+  n.getParam("/drop/w", goal.target_pose.pose.orientation.w);
 
+  ac.sendGoal(goal);
 
-int main(int argc, char** argv)
-{
-  // Initialize the simple_navigation_goals node
-  ros::init(argc, argv, "pick_drop");
-  ROS_INFO("PICK GOAL");
-  pose(-6.0);
-  ros::Duration(0.5).sleep();
-  ROS_INFO("DROP GOAL");
-  pose(10.0);
-  
-  
+  ac.waitForResult();
+
+  if (ac.getState()==actionlib::SimpleClientGoalState::SUCCEEDED)
+  {
+    delivery_data.data = 3;
+    pub.publish(delivery_data);
+    ROS_INFO("PICK UP REACHED");
+  }
+  else{
+    delivery_data.data = 2;
+    pub.publish(delivery_data);
+    ROS_INFO("Rusty did not reach the location");
+  }
   return 0;
 }
