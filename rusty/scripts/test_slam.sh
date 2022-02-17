ad29891f7c3b0c52cada8160c3482a51849be63b
SIDDARTH.D <siddarth.dayasagar@gmail.com>
2022-02-17 16:51:14 +0530

    Update test_slam.sh

--- rusty/scripts/test_slam.sh
+++ rusty/scripts/test_slam.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xterm  -e  " roslaunch rusty gazebo.launch " &
 sleep 5
-xterm  -e  " roslaunch rusty gmapping.launch" & 
+xterm  -e  " roslaunch rusty kartoSLAM.launch" & 
 sleep 5
 xterm  -e  " roslaunch rusty teleop_key.launch" 
