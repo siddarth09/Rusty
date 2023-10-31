8bfb0d7f08fc6fff0b69426ee1c970517b787398
Siddarh09 <siddarth.dayasagar@gmail.com>
2023-10-31 11:19:50 +0530

    updated with world

--- rusty_description/launch/gazebo.launch.py
+++ rusty_description/launch/gazebo.launch.py
@@ -7,7 +7,7 @@ from launch_ros.descriptions import ParameterValue
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='rusty_description').find('rusty_description')
-    #world_path=os.path.join(pkg_share, 'worlds/room2.sdf'),
+    world_path = 'src/Rusty/rusty_description/worlds/house.sdf'
     default_model_path = os.path.join(pkg_share, 'urdf/rusty.xacro')
     use_sim_time = LaunchConfiguration('use_sim_time')
     robot_state_publisher_node = launch_ros.actions.Node(
@@ -24,7 +24,7 @@ def generate_launch_description():
 
     return launch.LaunchDescription([
         launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 
-                                            'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'], 
+                                            'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so',world_path], 
                                            output='screen'),
         launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
                                 description='Flag to enable use_sim_time'),
